b15410100dc11de922e821d19473466a21f85bc2
manuj4189@gmail.com <manuj4189@gmail.com>
2024-09-10 22:22:50 +0530

    project initial commit

---
+++ src/main/java/in/ashokit/service/DashboardServiceImpl.java
@@ -0,0 +1,22 @@
+package in.ashokit.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import in.ashokit.dto.QuoteApiResponseDTO;
+@Service
+public class DashboardServiceImpl implements DashboardService {
+	private String quoteApiURL="https://dummyjson.com/quotes/random";
+
+	@Override
+	public 	QuoteApiResponseDTO getQuote() {
+		
+		RestTemplate rt=new RestTemplate();
+		ResponseEntity<QuoteApiResponseDTO> forEntity=rt.getForEntity(quoteApiURL, QuoteApiResponseDTO.class);
+		
+		QuoteApiResponseDTO body=forEntity.getBody();
+		return body;
+	}
+ 
+}
